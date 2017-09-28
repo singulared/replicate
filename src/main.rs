3987d8c61375c74b91299d7b857c493f0c270321
Belousow Makc <lib.bmw@gmail.com>
2017-09-28 20:29:43 +0300

    Use hyper instead of tokio-minihttp

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,42 @@
-extern crate env_logger;
+extern crate hyper;
 extern crate futures;
-extern crate tokio_minihttp;
-extern crate tokio_proto;
-extern crate tokio_service;
 
-use std::io;
+use futures::future::Future;
 
-use futures::future;
-use tokio_minihttp::{Request, Response, Http};
-use tokio_proto::TcpServer;
-use tokio_service::Service;
+use hyper::header::ContentLength;
+use hyper::server::{Http, Request, Response, Service};
 
-struct StatusService;
+struct ReplicateService;
 
-impl Service for StatusService {
+// const PHRASE: &'static str = "Hello, World!";
+
+impl Service for ReplicateService {
+    // boilerplate hooking up hyper's server types
     type Request = Request;
     type Response = Response;
-    type Error = io::Error;
-    type Future = future::Ok<Response, io::Error>;
+    type Error = hyper::Error;
+    // The future representing the eventual Response your call will
+    // resolve to. This can change to whatever Future you need.
+    type Future = Box<Future<Item=Self::Response, Error=Self::Error>>;
 
     fn call(&self, _request: Request) -> Self::Future {
-        println!("Request debug: {:?}", _request);
-        let method = _request.method();
-        // let path = _request.path();
-        let mut resp = Response::new();
-        // let mut body = format!("{}{}", method, path);
-        resp.status_code(200, "OK");
-        resp.body(method);
-        println!("+ debug: {:?}", _request);
-        future::ok(resp)
+        // println!("Request debug {:?}", _request);
+        
+        // We're currently ignoring the Request
+        // And returning an 'ok' Future, which means it's ready
+        // immediately, and build a Response with the 'PHRASE' body.
+        let body = format!("Method: {method}\nURI: {uri}", method=_request.method(), uri=_request.uri());
+
+        Box::new(futures::future::ok(
+            Response::new()
+                .with_header(ContentLength(body.len() as u64))
+                .with_body(body)
+        ))
     }
 }
 
 fn main() {
-    drop(env_logger::init());
-    let addr = "0.0.0.0:8080".parse().unwrap();
-    TcpServer::new(Http, addr)
-        .serve(|| Ok(StatusService));
+    let addr = "127.0.0.1:8080".parse().unwrap();
+    let server = Http::new().bind(&addr, || Ok(ReplicateService)).unwrap();
+    server.run().unwrap();
 }
