57832121b3172b05bfff76ef6194ca6ecbed1813
Belousow Makc <lib.bmw@gmail.com>
2018-05-10 20:20:45 +0300

    Migrate to pcap

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,30 @@
-extern crate hyper;
-extern crate futures;
+extern crate pcap;
 
-use futures::future::Future;
-
-use hyper::header::ContentLength;
-use hyper::server::{Http, Request, Response, Service};
-use hyper::Error;
-
-struct ReplicateService;
-
-
-impl Service for ReplicateService {
-    // boilerplate hooking up hyper's server types
-    type Request = Request;
-    type Response = Response;
-    type Error = Error;
-
-    // The future representing the eventual Response your call will
-    // resolve to. This can change to whatever Future you need.
-    type Future = Box<Future<Item=Self::Response, Error=Self::Error>>;
-
-    fn call(&self, request: Request) -> Self::Future {
-        // println!("Request debug {:?}", _request);
-        
-        // We're currently ignoring the Request
-        // And returning an 'ok' Future, which means it's ready
-        // immediately, and build a Response with the 'PHRASE' body.
-        let body = format!("Method: {method}\nURI: {uri}", method=request.method(), uri=request.uri());
-
-        Box::new(futures::future::ok(
-            Response::new()
-                .with_header(ContentLength(body.len() as u64))
-                .with_body(body)
-        ))
-    }
-}
 
 fn main() {
-    let addr = "127.0.0.1:8080".parse().unwrap();
-    let server = Http::new().bind(&addr, || Ok(ReplicateService)).unwrap();
-    server.run().unwrap();
+    for device in pcap::Device::list().unwrap() {
+        println!("Interface: {:?}", device);
+
+        // now you can create a Capture with this Device if you want.
+        // let mut cap = device.open().unwrap();
+
+        // get a packet from this capture
+        // let packet = cap.next();
+
+        // println!("got a packet! {:?}", packet);
+    }
+
+    // get the default Device
+    // let mut cap = pcap::Device::lookup().unwrap().open().unwrap();
+    let mut cap = pcap::Capture::from_device("lo").unwrap().open().unwrap();
+    cap.filter("port 8000").unwrap();
+
+    // get 10 packets
+    for _ in 0..100 {
+        let packet = cap.next().ok();
+        let data = String::from_utf8_lossy(packet.unwrap().data);
+        println!("{}", data);
+    }
+    let stats = cap.stats().unwrap();
+    println!("Received: {}, dropped: {}, if_dropped: {}", stats.received, stats.dropped, stats.if_dropped);
 }
